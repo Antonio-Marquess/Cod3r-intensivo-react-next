9f24f98db2ea90a1aff2e31c9fae487ac551f710
Antonio Marques <antonio.marquess@outlook.com.br>
2023-05-07 18:47:44 -0300

    Rederização condicional

--- app-inicial/src/components/condicional/Faq.tsx
+++ app-inicial/src/components/condicional/Faq.tsx
@@ -1,11 +1,47 @@
 import Pergunta from "./Pergunta";
+import { useState } from "react";
+
 export default function Faq() {
-    return (
-    <div className="flex flex-col gap-2">
-        <Pergunta />
-        <Pergunta />
-        <Pergunta />
-        <Pergunta />
-    </div>
-  );
+    const [ativo, setAtivo] = useState<number>(0)
+
+    function alternarVisibilidade(indice: number) {
+        if (indice === ativo) {
+            setAtivo(-1)
+        } else {
+            setAtivo(indice)
+        }
+    }
+        return (
+            <div className="flex flex-col gap-2 w-[90%] md:w-3/4">
+                <Pergunta
+                    indice={0}
+                    aberta={ativo === 0}
+                    texto="O que é um loop?"
+                    resposta="Repetição de instruções em um código" 
+                    alternarVisibilidade={alternarVisibilidade}
+                />
+                <Pergunta 
+                    indice={1}
+                    aberta={ativo === 1}
+                    texto="O que é um array?" 
+                    resposta="Coleção de elementos do mesmo tipo." 
+                    alternarVisibilidade={alternarVisibilidade}
+                />
+                <Pergunta 
+                    indice={2}
+                    aberta={ativo === 2}
+                    texto="O que é uma API?"
+                    resposta="Interface para interação entre sistemas." 
+                    alternarVisibilidade={alternarVisibilidade}
+                />
+                <Pergunta
+                    indice={3}
+                    aberta={ativo === 3}
+                    texto="O que é uma variável?" 
+                    resposta="Local de armazenamento para dados manipulados."
+                    alternarVisibilidade={alternarVisibilidade}
+                />
+            </div>
+        );
+    
 }   
--- app-inicial/src/components/condicional/Pergunta.tsx
+++ app-inicial/src/components/condicional/Pergunta.tsx
@@ -1,15 +1,41 @@
-export default function Pergunta() {
-    return (
-        <div className={`
-                border border-purple-800 rounded-md
-            `}>
+import If from "./if"
+import { IconChevronDown, IconChevronUp } from "@tabler/icons-react"
+import IfElse from "./IfElse"
 
-            <div className="bg-zinc-500 p-5 cursor-pointer">
-                Pergunta
+interface PerguntaProps {
+    indice: number
+    texto: string
+    resposta: string
+    aberta: boolean
+    alternarVisibilidade: (indice: number) => void
+}
+
+export default function Pergunta(props: PerguntaProps) {
+    return (
+        <div 
+            className={`
+                border border-purple-900 rounded-md overflow-hidden
+            `}
+        >
+            <div
+                className="bg-zinc-500 p-5 cursor-pointer select-none flex justify-between"
+                onClick={() =>props.alternarVisibilidade(props.indice)}
+            >
+                <span>{props.texto}</span>
+                {props.aberta ? <IconChevronDown /> : <IconChevronUp />}
+
+                {/* <IfElse teste={aberta}>
+                    <IconChevronUp />
+                    <IconChevronDown />
+                </IfElse> */}
+
             </div>
-            <div className="p-5">
-                Resposta
-            </div>
+            <If teste={props.aberta}>
+
+                <div className="p-5">
+                    {props.resposta}
+                </div>
+            </If>
         </div>
     )
 }
